7056f51cd691100596321d193b30aa6496f4811a
RedJanvier <janvierntwali@gmail.com>
2020-06-11 20:17:55 +0200

    release(# 1): frontend with react & react-router

---
+++ precache-manifest.ed211b8e2a9b2822aa7ac4c6ef776a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4806c24ec7a84c28dddb9cd9d0567e7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "774e5288a341769d1e61",
+    "url": "/static/css/main.2b063e6a.chunk.css"
+  },
+  {
+    "revision": "f86bfefbfad20dfcbbd1",
+    "url": "/static/js/2.f5c9dbb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f5c9dbb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774e5288a341769d1e61",
+    "url": "/static/js/main.e7c2ce64.chunk.js"
+  },
+  {
+    "revision": "849d81ef3c095827dd72",
+    "url": "/static/js/runtime-main.d1ca3a66.js"
+  }
+]);
